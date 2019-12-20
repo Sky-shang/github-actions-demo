88c01205bd1f1763a8dcf1c7ec0e958568abffc9
yuankun.shang <yuankun.shang@GLB-982>
2019-12-20 14:05:39 +0800

    demo

--- github-actions-demo/Models/demo.cs
+++ github-actions-demo/Models/demo.cs
@@ -16,6 +16,48 @@ namespace github_actions_demo.Models
         public int MyProperty6 { get; set; }
         public int MyProperty7 { get; set; }
         public int MyProperty8 { get; set; }
-
+        public int MyProperty9 { get; set; }
+        public int MyProperty10 { get; set; }
+        public int MyProperty11 { get; set; }
+        public int MyProperty12 { get; set; }
+        public int MyProperty13 { get; set; }
+        public int MyProperty14 { get; set; }
+        public int MyProperty15 { get; set; }
+        public int MyProperty16 { get; set; }
+        public int MyProperty17 { get; set; }
+        public int MyProperty18 { get; set; }
+        public int MyProperty19 { get; set; }
+        public int MyProperty20 { get; set; }
+        public int MyProperty21 { get; set; }
+        public int MyProperty22 { get; set; }
+        public int MyProperty23 { get; set; }
+        public int MyProperty24{ get; set; }
+        public int MyProperty25 { get; set; }
+        public int MyProperty26 { get; set; }
+        public int MyProperty27 { get; set; }
+        public int MyProperty28 { get; set; }
+        public int MyProperty29 { get; set; }
+        public int MyProperty30 { get; set; }
+        public int MyProperty31 { get; set; }
+        public int MyProperty32{ get; set; }
+        public int MyProperty33 { get; set; }
+        public int MyProperty34 { get; set; }
+        public int MyProperty35 { get; set; }
+        public int MyProperty36 { get; set; }
+        public int MyProperty37 { get; set; }
+        public int MyProperty38 { get; set; }
+        public int MyProperty39 { get; set; }
+        public int MyProperty40 { get; set; }
+        public int MyProperty41 { get; set; }
+        public int MyProperty42 { get; set; }
+        public int MyProperty43 { get; set; }
+        public int MyProperty44 { get; set; }
+        public int MyProperty45 { get; set; }
+        public int MyProperty46 { get; set; }
+        public int MyProperty47 { get; set; }
+        public int MyProperty48 { get; set; }
+        public int MyProperty49 { get; set; }
+        public int MyProperty50 { get; set; }
+                 
     }
 }
