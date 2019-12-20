fe1760dd7403044ad5153734b1aeb935b7ed4a3a
yuankun.shang <yuankun.shang@GLB-982>
2019-12-20 16:46:32 +0800

    数学名重复

--- github-actions-demo/Models/demo.cs
+++ github-actions-demo/Models/demo.cs
@@ -7,7 +7,7 @@ namespace github_actions_demo.Models
 {
     public class demo
     {
-        public int MyProperty { get; set; }
+        public int MyProperty1 { get; set; }
         public int MyProperty1 { get; set; }
         public int MyProperty2 { get; set; }
         public int MyProperty3 { get; set; }
