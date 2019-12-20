58773d5ed884196a7fb643575e57847d26f69ed2
yuankun.shang <yuankun.shang@GLB-982>
2019-12-20 13:59:08 +0800

    demo

---
+++ github-actions-demo/Models/demo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace github_actions_demo.Models
+{
+    public class demo
+    {
+        public int MyProperty { get; set; }
+        public int MyProperty1 { get; set; }
+        public int MyProperty2 { get; set; }
+        public int MyProperty3 { get; set; }
+        public int MyProperty4 { get; set; }
+        public int MyProperty5 { get; set; }
+        public int MyProperty6 { get; set; }
+        public int MyProperty7 { get; set; }
+        public int MyProperty8 { get; set; }
+
+    }
+}
